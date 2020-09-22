544af976991e86d7170f458c324f5a5074eba4b4
Tony Ng <tonyng987@gmail.com>
2020-09-22 15:01:02 -0700

    Allow read

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+function Todo({ todo }) {
+  return <div className="todo">{todo.text}</div>;
+}
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
   );
 }
+
+export default App;
