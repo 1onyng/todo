a662ef3d4b7e98c6c2a50c6064c86d7d7a098b14
Tony Ng <tonyng987@gmail.com>
2020-09-23 10:00:01 -0700

    Add create to-do

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,35 @@ function Todo({ todo }) {
   return <div className="todo">{todo.text}</div>;
 }
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
 function App() {
   const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
 
   return (
     <div className="app">
@@ -14,6 +41,7 @@ function App() {
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
